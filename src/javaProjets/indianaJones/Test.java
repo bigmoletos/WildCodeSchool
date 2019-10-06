a4b8905441b436282f103124eae85e23889828da
bigmoletos <bigmoletos@yopmail.com>
2019-10-06 11:49:07 +0200

    modif indiana, method fizzbuzz

--- src/javaProjets/indianaJones/Test.java
+++ src/javaProjets/indianaJones/Test.java
@@ -23,28 +23,19 @@ public class Test {
 	 *
 	 **/
 	public static void main(String[] args) {
-		String movie1 = "Indiana Jones and the Raiders of the Lost Ark";
-		String movie2 = "Indiana Jones and the Temple of Doom";
-		String movie3 = "Indiana Jones and the Last Crusade";
-		String cast1 = "Harrison Ford, " + "Karen Allen, " + "Paul Freeman";
-		String cast2 = "Harrison Ford, " + "Kate Capshow, " + "Jonathan Ke Quan";
-		String cast3 = "Harrison Ford, " + "Sean Connery, " + "Denholm Elliott";
+		String[] movies = { "Indiana Jones et le Royaume du Crâne de Cristal", "Indiana Jones et la Dernière Croisade",
+				"Indianan Jones et le Temple maudit" };
 
-		String[] movie = { movie1, movie2, movie3 };
-		String[] cast = { cast1, cast2, cast3 };
+		String[] actorsRCC = { "Harrison Ford", "Cate Blanchett", "Karen Allen" };
+		String[] actorsDC = { "Harrison Ford", "Sean Connery", "Denholm Elliott" };
+		String[] actorsTM = { "Harrison Ford", "Kate Capshaw", "Jonathan Ke Quan" };
 
-//		String[][] casting;
-//		casting = new String[][] { { movie1 + cast1 }, { movie2 + cast2 }, { movie3 + cast3 } };
-//
-//		System.out.println("Dans le film " + movie[0] + ", les principaux acteurs sont: " + cast[0]);
-//		System.out.println("Dans le film " + movie[1] + ", les principaux acteurs sont: " + cast[1]);
-//		System.out.println("Dans le film " + movie[2] + ", les principaux acteurs sont: " + cast[2]);
+		String[][] actors;
+		actors = new String[][] { actorsRCC, actorsDC, actorsTM };
 
-//		String[][] casting2 = { { cast1 }, { cast2 }, { cast3 } };
-		for (int i = 0; i < movie.length; i++) {
-			System.out.println("Dans le film " + movie[i] + ", les principaux acteurs sont: " + cast[i]);
+		for (int i = 0; i < movies.length; i++) {
+			System.out.println("Dans le film " + movies[i] + ", les principaux acteurs sont : " + actors[i][0] + ", "
+					+ actors[i][1] + " et " + actors[i][2] + ".");
 		}
-
 	}
-
 }
